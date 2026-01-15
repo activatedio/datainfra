b1a6158cc3a3c067e9393ab79e89e78c771003a8
Ben Tomasini <btomasini@activated.io>
2026-01-15 15:20:31 -0800

    updated types to handle keys at the base helper level

--- genlib/data/gorm/types.go
+++ genlib/data/gorm/types.go
@@ -1,17 +1,10 @@
 package gorm
 
 import (
-	"reflect"
-
 	"github.com/activatedio/datainfra/genlib/data"
 	"github.com/dave/jennifer/jen"
 	"github.com/gertd/go-pluralize"
 	"github.com/iancoleman/strcase"
-)
-
-const (
-	// BaseKeyId is the base key identifier used for accessing key fields.
-	BaseKeyId = "key" //nolint:revive // name is okay
 )
 
 // pl is an instance of pluralize.Client used for pluralizing and singularizing words in the application.
@@ -19,50 +12,17 @@ var (
 	pl = pluralize.NewClient()
 )
 
-// Key represents a struct with a Name and Type, primarily used for defining key fields in code generation.
-type Key struct {
-	Accessor jen.Code
-	Type     jen.Code
-	Name     string
-}
-
 // JenHelper represents a helper structure for managing data objects with metadata, table names, and keys.
 type JenHelper struct {
 	data.JenHelper
 	TablePrefix      string
 	TableName        string
-	Keys             []Key
 	ContextScopeCode jen.Code
 }
 
 // GetGormJenHelper transforms a data.Entry into a JenHelper enriched with keys and a pluralized table name.
 func GetGormJenHelper(entry *data.Entry) JenHelper {
 	jh := entry.GetJenHelper()
-
-	var keys []Key
-
-	t := jh.KeyField.Type
-
-	switch t.Kind() {
-	case reflect.Struct:
-		// We can only have one key per field from the key type
-		for i := 0; i < t.NumField(); i++ {
-			f := t.Field(i)
-			keys = append(keys, Key{
-				Accessor: jen.Id(BaseKeyId).Dot(f.Name),
-				Type:     jen.Qual(f.Type.PkgPath(), f.Type.Name()),
-				Name:     f.Name,
-			})
-		}
-	case reflect.Ptr:
-		panic("key can't be a pointer type")
-	default:
-		keys = append(keys, Key{
-			Accessor: jen.Id(BaseKeyId),
-			Type:     jen.Qual(t.PkgPath(), t.Name()),
-			Name:     jh.KeyField.Name,
-		})
-	}
 
 	tableName := pl.Plural(strcase.ToSnake(jh.StructName))
 	var csc jen.Code
@@ -77,7 +37,6 @@ func GetGormJenHelper(entry *data.Entry) JenHelper {
 
 	return JenHelper{
 		JenHelper:        jh,
-		Keys:             keys,
 		TablePrefix:      strcase.ToSnake(jh.StructName),
 		TableName:        tableName,
 		ContextScopeCode: csc,
